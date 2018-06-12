0f8280ce71d93a565eecea6a04377c784ee9ad4d
kakaict <canhcung88@gmail.com>
2018-06-13 01:00:03 +0200

    init

---
+++ src/main/java/com/tqk/SpringBootMicroEurekaNamingServerApplication.java
@@ -0,0 +1,14 @@
+package com.tqk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootMicroEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroEurekaNamingServerApplication.class, args);
+	}
+}
